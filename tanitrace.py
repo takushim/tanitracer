79e21fc3b6a909d807a33fdd355d3d055800d6e4
Takushi Miyoshi <tmiyoshi@nidcd.nih.gov>
2019-04-03 17:41:38 -0400

    Added rerun option

--- tanitrace.py
+++ tanitrace.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
 import os, sys, argparse, pandas, numpy
-from taniclass import gaussian8, nnchaser, spotmarker
+from taniclass import gaussian8, nnchaser, spotmarker, spotplotter
 from skimage.external import tifffile
 
 # prepare library instances
 tracer = gaussian8.Gaussian8()
 chaser = nnchaser.NNChaser()
 marker = spotmarker.SpotMarker()
+plotter = spotplotter.SpotPlotter()
 
 # defaults
 input_filename = None
+import_settings_filename = None
 output_tsv_filename = None
 output_image_filename = None
 chase_spots = False
@@ -20,23 +22,31 @@ output_image = False
 parser = argparse.ArgumentParser(description='trace spots using gaussian fitting.', \
                                  epilog='use "ls *.stk | foreach -process{tanitracer [options] $_.fullname}"', \
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-f', '--output-tsv-file', nargs=1, default = None, \
+parser.add_argument('-T', '--output-tsv-file', nargs=1, default = None, \
                     help='output tsv file name ([basename].txt if not specified)')
 
-parser.add_argument('-l', '--laplace', nargs=1, type=float, default=[tracer.laplace], \
+# import settings
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-R', '--rerun', action='store_true', default=False, \
+                   help='import settings finding [basename].txt')
+group.add_argument('-I', '--import-settings-file', nargs=1, default = None, \
+                   help='import settings from other results (overwritten by options)')
+
+# append are used to judge whether or not options are set
+parser.add_argument('-l', '--laplace', nargs=1, type=float, default=[tracer.laplace], action='append',\
                     help='maximum spot diameter to filter noise')
-parser.add_argument('-m', '--min-distance', nargs=1, type=int, default=[tracer.min_distance], \
+parser.add_argument('-m', '--min-distance', nargs=1, type=int, default=[tracer.min_distance], action='append',\
                     help='pixel area to find local max (usually 1.0)')
-parser.add_argument('-t', '--threshold-abs', nargs=1, type=float, default=[tracer.threshold_abs], \
+parser.add_argument('-t', '--threshold-abs', nargs=1, type=float, default=[tracer.threshold_abs], action='append',\
                     help='threshold to find local max')
-parser.add_argument('-x', '--max-diameter', nargs=1, type=float, default=[tracer.max_diameter], \
+parser.add_argument('-x', '--max-diameter', nargs=1, type=float, default=[tracer.max_diameter], action='append',\
                     help='maximum diameter of spots')
-parser.add_argument('-u', '--dup-threshold', nargs=1, type=float, default=[tracer.dup_threshold], \
+parser.add_argument('-u', '--dup-threshold', nargs=1, type=float, default=[tracer.dup_threshold], action='append',\
                     help='minimum distance to distinguish two spots')
 
 parser.add_argument('-C', '--chase-spots', action='store_true', default=chase_spots, \
                     help='chase spots before output tsv file')
-parser.add_argument('-d', '--chase-distance', nargs=1, type=float, default = [chaser.chase_distance], \
+parser.add_argument('-d', '--chase-distance', nargs=1, type=float, default = [chaser.chase_distance], action='append',\
                     help='maximum distance to assume as identical spots (pixel)')
 
 parser.add_argument('-O', '--output-image', action='store_true', default=output_image, \
@@ -67,14 +77,34 @@ if args.output_tsv_file is None:
 else:
     output_tsv_filename = args.output_tsv_file[0]
 
-tracer.laplace = args.laplace[0]
-tracer.min_distance = args.min_distance[0]
-tracer.threshold_abs = args.threshold_abs[0]
-tracer.max_diameter = args.max_diameter[0]
-tracer.dup_threshold = args.dup_threshold[0]
+# load options from the previous result
+if args.rerun is True:
+    args.import_settings_file = os.path.splitext(os.path.basename(input_filename))[0] + '.txt'
 
-chase_spots = args.chase_spots
-chaser.chase_distance = args.chase_distance[0]
+if args.import_settings_file is not None:
+    import_settings_filename = args.import_settings_file[0]
+    print("Importing settings: %s (ignoring other options)" % (import_settings_filename))
+    fileext = os.path.splitext(os.path.basename(import_settings_filename))[1].lower()
+    if (fileext == '.stk') or (fileext == '.tif'):
+        import_settings_filename = os.path.splitext(os.path.basename(import_settings_filename))[0] + '.txt'
+        print("Reading %s instead of %s." % (import_settings_filename, args.import_settings_file[0]))
+
+    params = plotter.read_image_params(import_settings_filename)
+    # pretend as if options are set
+    for key in params:
+        if hasattr(args, key) and len(getattr(args, key)) > 1:
+            getattr(args, key).append(params[key])
+        if key == 'chase_distance':
+            chase_spots = True
+
+tracer.laplace = args.laplace[-1]
+tracer.min_distance = args.min_distance[-1]
+tracer.threshold_abs = args.threshold_abs[-1]
+tracer.max_diameter = args.max_diameter[-1]
+tracer.dup_threshold = args.dup_threshold[-1]
+
+chase_spots = (args.chase_spots | chase_spots)
+chaser.chase_distance = args.chase_distance[-1]
 
 marker.marker_colors = args.marker_colors
 marker.marker_size = args.marker_size[0]
