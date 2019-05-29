3ba0f6038f32b4e0eda35444c93d38ce0ea2c29e
Takushi Miyoshi <tmiyoshi@nidcd.nih.gov>
2019-05-29 16:34:16 -0400

    Bug fix in tracking when planes do not contain spots

--- taniclass/nnchaser.py
+++ taniclass/nnchaser.py
@@ -19,43 +19,53 @@ class NNChaser:
             # get partial spot table
             orig_spots = spot_table[spot_table.plane == index]
             next_spots = spot_table[spot_table.plane == index + 1]
-            if (len(orig_spots) == 0) or (len(next_spots) == 0):
+            if len(orig_spots) == 0:
                 results.append(numpy.array([]))
                 continue
 
-            # nearest neighbor to find nearest spots
-            nn = NearestNeighbors(n_neighbors = 1, metric = 'euclidean').fit(next_spots[['x', 'y']].values)
-            distances, targets = nn.kneighbors(orig_spots[['x', 'y']].values)
-
-            # make numpy array to find duplicates and too far spots
             pairs = numpy.zeros(len(orig_spots), dtype=[('orig_array_index', numpy.int), \
                                                         ('orig_total_index', numpy.int), \
                                                         ('next_array_index', numpy.int), \
                                                         ('distance', numpy.float), \
                                                         ('track_distance', numpy.float), \
                                                         ('valid', numpy.bool)])
-            pairs['orig_array_index'] = numpy.arange(len(orig_spots))
-            pairs['orig_total_index'] = orig_spots.total_index.values
-            pairs['next_array_index'] = targets.flatten()
-            pairs['distance'] = distances.flatten()
-            pairs['track_distance'] = 0.0
-            pairs['valid'] = True
 
-            # omit too far spots
-            pairs['valid'][pairs['distance'] > self.chase_distance] = False
+            if len(next_spots) == 0:
+                # do not run nn since there are no spots on the next plane
+                pairs['orig_array_index'] = numpy.arange(len(orig_spots))
+                pairs['orig_total_index'] = orig_spots.total_index.values
+                pairs['next_array_index'] = -1
+                pairs['distance'] = 0.0
+                pairs['track_distance'] = 0.0
+                pairs['valid'] = False
+            else:
+                # nearest neighbor to find nearest spots
+                nn = NearestNeighbors(n_neighbors = 1, metric = 'euclidean').fit(next_spots[['x', 'y']].values)
+                distances, targets = nn.kneighbors(orig_spots[['x', 'y']].values)
 
-            # find duplicated targets
-            pairs = numpy.sort(pairs, order=['next_array_index', 'distance'])
-            unique_index = numpy.unique(pairs['next_array_index'], return_index = True)[1]
+                # make numpy array to find duplicates and too far spots
+                pairs['orig_array_index'] = numpy.arange(len(orig_spots))
+                pairs['orig_total_index'] = orig_spots.total_index.values
+                pairs['next_array_index'] = targets.flatten()
+                pairs['distance'] = distances.flatten()
+                pairs['track_distance'] = 0.0
+                pairs['valid'] = True
 
-            # omit duplicates
-            mask = numpy.ones(len(pairs), dtype=numpy.bool)
-            mask[unique_index] = False
-            pairs['valid'][mask] = False
+                # omit too far spots
+                pairs['valid'][pairs['distance'] > self.chase_distance] = False
 
-            # delete next_index and distance
-            pairs['next_array_index'][pairs['valid'] == False] = -1
-            pairs['distance'][pairs['valid'] == False] = 0.0
+                # find duplicated targets
+                pairs = numpy.sort(pairs, order=['next_array_index', 'distance'])
+                unique_index = numpy.unique(pairs['next_array_index'], return_index = True)[1]
+
+                # omit duplicates
+                mask = numpy.ones(len(pairs), dtype=numpy.bool)
+                mask[unique_index] = False
+                pairs['valid'][mask] = False
+
+                # delete next_index and distance
+                pairs['next_array_index'][pairs['valid'] == False] = -1
+                pairs['distance'][pairs['valid'] == False] = 0.0
 
             # sort again and save
             pairs = numpy.sort(pairs, order=['orig_array_index'])
@@ -81,6 +91,7 @@ class NNChaser:
         for index in range(max(spot_table.plane)):
             orig_pairs = results[index]
             next_pairs = results[index + 1]
+
             if (len(orig_pairs) == 0) or (len(next_pairs) == 0):
                 continue
 
@@ -90,8 +101,8 @@ class NNChaser:
             next_pairs['track_distance'][next_indexes] = orig_pairs['distance'][orig_indexes]
 
         # update indexes and make a distance column
-        spot_table['total_index'] = numpy.concatenate([result['orig_total_index'] for result in results])
-        spot_table['distance'] = numpy.concatenate([result['track_distance'] for result in results])
+        spot_table['total_index'] = numpy.concatenate([result['orig_total_index'] for result in results if len(result) > 0])
+        spot_table['distance'] = numpy.concatenate([result['track_distance'] for result in results if len(result) > 0])
 
         # sort table
         spot_table = spot_table.sort_values(by = ['total_index', 'plane']).reset_index(drop=True)
